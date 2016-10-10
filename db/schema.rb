56e676f06b2dca11a086672568354f4dc55b0e45
joanna broad <joanna.broad93@gmail.com>
2016-10-10 16:24:00 +0100

    price

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161004160308) do
+ActiveRecord::Schema.define(version: 20161010140203) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20161004160308) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "color"
+    t.integer  "price"
   end
 
 end
