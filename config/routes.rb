5f3b3ec166549a69e763899d09abd161ce9324d1
joanna broad <joanna.broad93@gmail.com>
2016-10-12 16:30:12 +0100

    m

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,8 @@ Rails.application.routes.draw do
 
   resources :orders, only: [:index, :show, :create, :destroy]
 
-root "static_pages#landing_page"
+  root "static_pages#landing_page"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
