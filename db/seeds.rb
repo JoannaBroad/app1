3d72db23c774574d674ce2a995d7eae594160841
joanna broad <joanna.broad93@gmail.com>
2016-10-17 09:51:46 +0100

    blue

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,5 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Product.create(:name => "seed-generated product", :description => "I also created this product without using the HTML form!", :color =>blue)
-Product.create(:name => "seed-generated product", :description => "I also created this product without using the HTML form!", :color =>blue)
+Product.create(:name => "seed-generated product", :description => "I also created this product without using the HTML form!", :color => "")
+Product.create(:name => "seed-generated product", :description => "I also created this product without using the HTML form!", :color => "")
