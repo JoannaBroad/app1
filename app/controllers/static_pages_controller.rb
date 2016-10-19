f082689293eded58509858b513281c258c6b6a26
joanna broad <joanna.broad93@gmail.com>
2016-10-19 13:25:38 +0100

    carousel

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,15 @@ class StaticPagesController < ApplicationController
   def index
   end
 
-  def landing_page
-    @featured_product = Product.first
-  end
+#   def landing_page
+#     @featured_product = Product.first
+#   end
+# end
+
+def landing_page
+  @products = Product.limit(3)
+end
+
+
+
 end
