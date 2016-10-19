c6e4994f09ed110e79b0c2aa49f55b095de04bfc
joanna broad <joanna.broad93@gmail.com>
2016-10-19 11:00:12 +0100

    u

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Product.create(:name => "Birthday card", :description => "dementions", :image_url => "birthday.jpg", :color => "purple")
-Product.create(:name => "Winnie the Pooh", :description => "dementions", :image_url => "winne.jpg", :color => "yellow")
-Product.create(:name => "Lady in Pink", :description => "dementions", :image_url => "lady.jpg", :color => "pink")
-Product.create(:name => "Mens On your Special Day", :description => "dementions", :image_url => "train.jpg", :color => "brown")
+Product.create(:name => "Birthday card", :description => "dementions", :image_url => "birthday.jpg", :colour => "purple")
+Product.create(:name => "Winnie the Pooh", :description => "dementions", :image_url => "winne.jpg", :colour => "yellow")
+Product.create(:name => "Lady in Pink", :description => "dementions", :image_url => "lady.jpg", :colour => "pink")
+Product.create(:name => "Mens On your Special Day", :description => "dementions", :image_url => "train.jpg", :colour => "brown")
