18ff5074156d996ea5649ae2af71927bd54ed112
joanna broad <joanna.broad93@gmail.com>
2016-11-08 19:33:56 +0000

    sendgrid

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,15 +2,19 @@ class StaticPagesController < ApplicationController
   def index
   end
 
-#   def landing_page
-#     @featured_product = Product.first
-#   end
-# end
-
 def landing_page
   @products = Product.limit(5)
 end
 
+def thank_you
+  @name = params[:name]
+  @email = params[:email]
+  @message = params[:message]
+  ActionMailer::Base.mail(:from => @email,
+      :to => 'joanna.broad93@gmail.com',
+      :subject => "A new contact form message from nill",
+      :body => @message).deliver_now
+end
 
 
 end
