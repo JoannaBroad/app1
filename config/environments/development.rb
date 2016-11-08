566c3e3293f1fa5a0996c2929fa6aa64dbbf7cba
joanna broad <joanna.broad93@gmail.com>
2016-11-08 19:51:09 +0000

    email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,6 +29,17 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
+  :enable_starttls_auto => true,
+  :address => 'smtp.gmail.com',
+  :port => 587,
+  :authentication => :plain,
+  :domain => 'https://debs-cards.herokuapp.com/',
+  :user_name => 'joanna.broad93@gmail.com',
+  :password => 'Ashlyb2014'
+}
+
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
