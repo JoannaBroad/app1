f1cc3a6676ade593870c25cba5f5c755432df5ab
joanna broad <joanna.broad93@gmail.com>
2016-11-09 13:00:23 +0000

    fix emails

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,7 +35,7 @@ config.action_mailer.default :charset => "utf-8"
 config.action_mailer.smtp_settings = {
   address: "smtp.gmail.com",
   port: 587,
-  domain: "debs-cards.herokuapp.com",
+  domain: "gmail.com",
   authentication: "plain",
   enable_starttls_auto: true,
   user_name: ENV["GMAIL_USERNAME"],
