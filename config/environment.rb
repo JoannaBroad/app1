bbcf26725f2f70c032700d1e7e14c46213f1b03a
joanna broad <joanna.broad93@gmail.com>
2016-11-09 14:59:34 +0000

    test

--- config/environment.rb
+++ config/environment.rb
@@ -3,14 +3,3 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-config.action_mailer.default_url_options = { :host => 'debs-cards.herokuapp.com' }
-config.action_mailer.smtp_settings = {
-  address: "smtp.gmail.com",
-  port: 587,
-  domain: "debs-cards.herokuapp.com",
-  authentication: "plain",
-  enable_starttls_auto: true,
-  user_name: ENV["GMAIL_SMTP_USER"],
-  password: ENV["GMAIL_SMTP_PASSWORD"]
-}
