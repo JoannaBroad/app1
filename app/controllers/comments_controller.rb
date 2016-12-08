04ca4de9c621529b3ad70624864c21ea9b417a32
joanna broad <joanna.broad93@gmail.com>
2016-12-08 14:48:56 +0000

    5.8

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,27 +1,19 @@
 class CommentsController < ApplicationController
 
-  def create
-    @product = Product.find(params[:product_id])
-    @comment = @product.comments.new(comment_params)
-    @comment.user = current_user
-    @comment.save
-    redirect_to product_path(@product)
-  end
+	def create
+		@product = Product.find(params[:product_id])
+		@comment = @product.comments.new(comment_params)
+		@comment.user = current_user
+		@comment.save
+		redirect_to product_path(@product)
+	end
 
-  def destroy
-
-  end
-
-  resources :products do
-    resources :comments
-  end
-  resources :users
+	def destroy
+	end
 
 private
-
-  def comment_params
-    params.require(:comment).permit(:user_id, :body, :rating)
-  end
-
+	def comment_params
+		params.require(:comment).permit(:user_id, :body, :rating)
+	end
 
 end
