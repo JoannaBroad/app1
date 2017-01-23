05f2141188b2eddd0fd0327ccbb719a7eb8312ac
joanna broad <joanna.broad93@gmail.com>
2017-01-23 12:19:54 +0000

    comment

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,23 +1,22 @@
 module ApplicationCable
-	class Connection < ActionCable::Connection::Base
-		identified_by :current_user
-
-		def connect
-
-		end
-
-		def disconnect
-			self.current_user = find_verified_user
-		end
-
-		protected
-
-		def find_verified_user
-			if verifyed_user = request.env['warden'].user
-				verifyed_user
-			else
-				reject_unautherorized_conection
-   			end
-		end
+  class Connection < ActionCable::Connection::Base
+  	identified_by :current_user
+  	def connect
+  		self.current_user = find_verified_user
 	end
+
+    def disconnect
+
+    end
+
+    protected
+
+    def find_verified_user
+      if verified_user = request.env['warden'].user
+        verified_user
+      else
+        reject_unauthorized_connection
+      end
+    end
+  end
 end
