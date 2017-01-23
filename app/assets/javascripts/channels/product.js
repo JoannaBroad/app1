c89fa3dec7ed2a5fcb9750ec7bfe26778e827a37
joanna broad <joanna.broad93@gmail.com>
2017-01-23 12:10:57 +0000

    comment

--- app/assets/javascripts/channels/product.js
+++ app/assets/javascripts/channels/product.js
@@ -1,15 +1,16 @@
+
 App.product = App.cable.subscriptions.create("ProductChannel", {
-  connected: function () {
-        // Called when the subscription is ready for use on the server
+  connected: function() {
+    // Called when the subscription is ready for use on the server
   },
 
-  disconnected: function () {
-        // Called when the subscription has been terminated by the server
+  disconnected: function() {
+    // Called when the subscription has been terminated by the server
   },
 
-  received: function (data) {
-        // Called when there's incoming data on the websocket for this channel
-    $(".alert.alert-info").show();
+  received: function(data) {
+		// Called when there's incoming data on the websocket for this channel
+	  $(".alert.alert-info").show();
 	  $('.product-reviews').prepend(data.comment);
 	  $("#average-rating").attr('data-score', data.average_rating);
 	  refreshRating();
@@ -20,8 +21,8 @@ App.product = App.cable.subscriptions.create("ProductChannel", {
   	    product_id: $("[data-product-id]").data("product-id")
   	});
 	}
-
+});
 
 $(document).on('turbolinks:load', function() {
   App.product.listen_to_comments();
- });
+});
