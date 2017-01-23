c025ff0cc3070a4834efaa2e40513cdab98a9a9a
joanna broad <joanna.broad93@gmail.com>
2017-01-23 11:43:16 +0000

    6,14

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,23 @@
 module ApplicationCable
-  class Connection < ActionCable::Connection::Base
-  end
+	class Connection < ActionCable::Connection::Base
+		identified_by :current_user
+
+		def connect
+
+		end
+
+		def disconnect
+			self.current_user = find_verified_user
+		end
+
+		protected
+
+		def find_verified_user
+			if verifyed_user = request.env['warden'].user
+				verifyed_user
+			else
+				reject_unautherorized_conection
+   			end
+		end
+	end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,11 +2,11 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :product
 
-validates :body, presence: true
+  validates :body, presence: true
   validates :user, presence: true
   validates :product, presence: true
   validates :rating, numericality: { only_integer: true }
-
+  after_create_commit { CommentUpdateJob.preform_later(self, @user)}
 
 scope :rating_desc, -> { order(rating: :desc) }
 
